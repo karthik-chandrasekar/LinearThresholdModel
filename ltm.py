f5145b7a3650614877c8aaf445dfae451e3dd7f1
Karthik Chandrasekar <karthikchandrasekar@Karthiks-MacBook-Pro.local>
2013-11-13 20:03:14 -0700

    Added preprocessing part and activation count finding part

--- ltm.py
+++ ltm.py
@@ -8,6 +8,9 @@ class ltm:
         self.dir_name = "input"
         self.file_name = "amazon0302.txt"
         self.logger_file = "ltm.log"
+
+        self.NODE_THRESHOLD = 0.5
+
         self.edge_to_weight_dict = {}
         self.node_to_incoming_edge_dict = {}
         self.node_to_activation_count_dict = {}    
@@ -45,18 +48,46 @@ class ltm:
                 edge_name = node+"TO"+neighbor
                 self.edge_to_weight_dict[edge_name] = edge_weight
                 self.node_to_incoming_edge_dict.setdefault(neighbor, []).append(edge_name)        
-        import pdb;pdb.set_trace()
 
     def run(self):
         for node in self.nodes_list:
             if not node:
                 continue
+            logging.info("Finding  activation count for %s" % (node))
             count = self.find_activation_count(node) 
             self.node_to_activation_count_dict[node] = count 
 
     def find_activation_count(self, node):
-        pass               
+        activation_count = 0
+        neighbors_list = self.G.neighbors(node)
 
+        for neighbor in neighbors_list:
+            is_activated = self.check_if_activated(neighbor)            
+
+            if is_activated:
+                activation_count = 1+self.find_activation_count(neighbor)              
+    
+        return activation_count 
+        
+    def check_if_activated(self, node):
+
+        total_edge_weight = 0
+
+        incoming_edge_list = self.node_to_incoming_edge_dict.get(node)
+        if not incoming_edge_list:
+            return False
+
+        for edge in incoming_edge_list:
+            edge_weight = self.edge_to_weight_dict.get(edge)
+            if not edge_weight:
+                continue
+
+            total_edge_weight += edge_weight
+         
+        if total_edge_weight >= self.NODE_THRESHOLD:
+            return True
+
+        return False
 
 if __name__ == "__main__":
     ltm_obj = ltm()
